81ccfcb513ff53718b09568068474dd1bc370037
Aluriak <lucas.bourneuf@laposte.net>
2018-04-18 01:22:12 +0200

    asp_model now handle user-defined objects

--- asp_model.py
+++ asp_model.py
@@ -1,7 +1,12 @@
 """Routines to build model from ASP data"""
 
 import clyngor
-from commons import Model, Orbit, uid_gen
+from commons import Model, Orbit, uid_gen, asp_value_to_pyvalue
+
+
+ORBIT_ARGS_ORDER = ('semimajoraxis', 'eccentricity', 'obliquity', 'inclination', 'angle')
+STAR_ARGS_ORDER = ('spectral_class', 'solar_radius', 'solar_mass')
+PLANET_ARGS_ORDER = ('cls', 'earth_radius', 'earth_mass')
 
 
 def data(fname:str) -> [dict]:
@@ -9,7 +14,7 @@ def data(fname:str) -> [dict]:
     yield from clyngor.solve(fname).by_predicate.parse_args.careful_parsing
 
 
-def root_info(asp_model) -> (str or tuple, str, str, ...):
+def root_info(asp_model, objects:dict) -> (str or tuple, str, str, ...):
     roots = tuple(args for args in asp_model.get('root', ()) if len(args) >= 2)
     if len(roots) > 1:
         print('Multiple roots. Bad.')
@@ -28,19 +33,42 @@ def root_info(asp_model) -> (str or tuple, str, str, ...):
         print('No root. Bad.')
         exit(1)  # TODO: determine the root by yourself
 
-    types, names = _types_and_names(asp_model)
+    types, names = _types_and_names(asp_model, objects)
     return types.get(root, root), name, uid, types, names, root
 
 
-def populate_orbits(asp_model, orbits:list, root_uid:str, types:dict, names:dict, root_number:int):
+def populate_orbits(asp_model, orbits:list, objects:dict, root_uid:str,
+                    types:dict, names:dict, root_number:int):
     names[root_number] = root_uid
     for args in asp_model.get('orbit', ()):
-        orbits.extend(gen_orbits(args, types, names))
+        orbits.extend(gen_orbits(args, objects, types, names))
 
 
-def gen_orbits(args:tuple, types:dict, names:dict) -> [Orbit]:
+def gen_orbits(args:tuple, objects:dict, types:dict, names:dict) -> [Orbit]:
     if len(args) >= 3:
-        parent, child, distance, *properties = args
+        parent, child, orbit_params, *properties = args
+
+        # Compute orbit parameters
+        if isinstance(orbit_params, (str, int)):
+            orbit_params_map['semimajoraxis'] = asp_value_to_pyvalue(orbit_params)
+        elif not isinstance(orbit_params, tuple) or len(orbit_params) != 2:
+            raise ValueError("Unexpected orbit parameters: {}".format(orbit_params))
+        else:  # it's a tuple !
+            predicat, orbit_params = orbit_params
+            orbit_params_map = {
+                k: asp_value_to_pyvalue(v)
+                for k, v in dict(zip(ORBIT_ARGS_ORDER, orbit_params)).items()
+            }
+        distance = orbit_params_map['semimajoraxis']  # shortcut
+
+        # Compute objects representation
+        if isinstance(child, tuple) and child[0] in {'planet', 'star'}:
+            child, obj = _asp_tuple_to_object(child)
+            objects[child] = obj
+        if isinstance(parent, tuple) and parent[0] in {'planet', 'star'}:
+            parent, obj = _asp_tuple_to_object(parent)
+            objects[parent] = obj
+
         properties = set(properties)
         retrograde = 'retrograde' in map(lambda s: s.strip('"'), properties)
         parent = names.get(parent, parent)
@@ -50,8 +78,6 @@ def gen_orbits(args:tuple, types:dict, names:dict) -> [Orbit]:
             except KeyError:
                 raise ValueError("Unknow type (is/2) for element of uid {}".format(child))
             child = names.setdefault(child, str(child) + str(uid_gen()))
-        if isinstance(distance, str):
-            distance = (float if '.' in distance else int)(distance.strip('"'))
         # orbit cases
         if isinstance(child, tuple) and child[0] == 'orbit':
             # recursive handling of the childs
@@ -63,6 +89,8 @@ def gen_orbits(args:tuple, types:dict, names:dict) -> [Orbit]:
             yield from gen_orbits(subargs, types, names)
         elif isinstance(child, tuple) and child[0] == 'ring':
             ring_params = child[1]
+            if 'angle' in orbit_params_map:
+                raise ValueError("Orbit parameters specify an angle, but the object is a ring.")
             assert len(child) == 2 and len(ring_params) >= 2
             ring_type, number, *properties = ring_params
             angle_step = 360 / number
@@ -71,7 +99,7 @@ def gen_orbits(args:tuple, types:dict, names:dict) -> [Orbit]:
                 angle = idx * angle_step
                 ring_orbits.append(
                     Orbit(names.get(parent, parent), ring_type, ring_type + str(uid_gen()),
-                          distance, angle=angle, isretrograde=retrograde)
+                          **orbit_params_map, angle=angle, isretrograde=retrograde)
                 )
             for prop in properties:
                 if len(prop) == 2 and prop[0] == 'orbit' and len(prop[1]) >= 3:
@@ -82,7 +110,8 @@ def gen_orbits(args:tuple, types:dict, names:dict) -> [Orbit]:
                         yield from gen_orbits((parent, *subargs[1:]), types, names)
             yield from ring_orbits
         elif isinstance(child, str):
-            yield Orbit(names.get(parent, parent), child_type, child, distance, isretrograde=retrograde)
+            yield Orbit(names.get(parent, parent), child_type, child,
+                        isretrograde=retrograde, **orbit_params_map)
         else:
             raise ValueError("Non handlable orbit data '{}' (understood as '{}')"
                              "".format(args, (parent, child, distance, properties)))
@@ -90,7 +119,7 @@ def gen_orbits(args:tuple, types:dict, names:dict) -> [Orbit]:
         print('Non valid: {}'.format(args))
 
 
-def _types_and_names(asp_model) -> (dict, dict):
+def _types_and_names(asp_model, objects:dict) -> (dict, dict):
     types = {}  # integer uid -> object types
     names = {}  # integer uid -> object name
     for args in asp_model.get('is', ()):
@@ -100,6 +129,18 @@ def _types_and_names(asp_model) -> (dict, dict):
                 raise ValueError("UID {} given multiple (at least two) times. "
                                  "One for '{}', One for '{}'"
                                  "".format(uid, types[uid], obj))
+            if isinstance(obj, tuple):
+                obj, obj_repr = _asp_tuple_to_object(obj)
+                objects[obj] = obj_repr
             types[uid] = obj
             names[uid] = obj + str(uid_gen())
     return types, names
+
+def _asp_tuple_to_object(atom:tuple) -> (str, dict):
+    """Return the unique id and the dict representation of given planet or star object"""
+    assert isinstance(atom, tuple) and atom[0] in {'planet', 'star'}
+    args_order = PLANET_ARGS_ORDER if atom[0] == 'planet' else STAR_ARGS_ORDER
+    new_uid = str(uid_gen())
+    data = dict(zip(args_order, atom[1]))
+    data['type'] = atom[0]
+    return new_uid, data
--- commons.py
+++ commons.py
@@ -4,12 +4,23 @@ from collections import namedtuple
 
 
 # Model structure
-Model = namedtuple('Model', 'system_name, root, root_uid, orbits')
+Model = namedtuple('Model', 'system_name, root, root_uid, orbits, objects')
 # Orbit structure
 Orbit = namedtuple('Orbit', 'parent, orbiter, orbiter_uid, semimajoraxis, eccentricity, inclination, angle, isretrograde')
-Orbit.__new__.__defaults__ = 0, 0, 0, False,
+Orbit.__new__.__defaults__ = 0, 0, 0, False
 
 
 # Generator of UID
 _uid_gen = itertools.count(1)
 uid_gen = lambda: next(_uid_gen)
+
+
+
+def asp_value_to_pyvalue(value:str or int) -> float or int:
+    if isinstance(value, str):
+        value = value.strip('"')
+        if value.count('.') == 1 and all(part.isnumeric() for part in value.split('.')):
+            return float(value)
+        elif value.isnumeric():
+            return int(value)
+    return value
